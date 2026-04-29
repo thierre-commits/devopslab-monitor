8a29023a9579c1758889cb338ac5dfb769a0de71
Thierre Almeida <ttimota02@gmail.com>
2026-04-29 11:04:33 -0300

    feat: add docker build to CI pipeline

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,3 +1,5 @@
+import os
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -8,6 +10,11 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+database_url = os.getenv("DATABASE_URL")
+
+if database_url:
+    config.set_main_option("sqlalchemy.url", database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
